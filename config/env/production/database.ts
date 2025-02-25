72d721ada6f81b4ec32026f5de7ca3728652ecec
lymbf <paw.czekaj@gmail.com>
2025-02-25 14:52:10 +0100

    again..

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('PGHOST', 'postgres.railway.internal'),
-            port: env.int('PGPORT', 5432),
+            host: env('PGHOST', '127.0.0.1'),
+            port: env.int('PGPORT', 5931),
             database: env('PGDATABASE', 'railway'),
             user: env('PGUSER', 'postgres'),
-            password: env('PGPASSWORD', 'ARGscwkXIwaDhdqXIaOBYFMysBGJzFtU'),
+            password: env('PGPASSWORD', 'password'),
             ssl: env.bool(true),
         },
     },
