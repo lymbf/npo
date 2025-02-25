dcdadb829c02a0e6c1695a3bf41eb084740a649a
lymbf <paw.czekaj@gmail.com>
2025-02-25 14:42:55 +0100

    retryn with vars

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('PGHOST'),
-            port: env.int('PGPORT'),
-            database: env('PGDATABASE'),
-            user: env('PGUSER'),
-            password: env('PGPASSWORD'),
+            host: env('PGHOST', 'postgres.railway.internal'),
+            port: env.int('PGPORT', 5432),
+            database: env('PGDATABASE', 'railway'),
+            user: env('PGUSER', 'postgres'),
+            password: env('PGPASSWORD', 'ARGscwkXIwaDhdqXIaOBYFMysBGJzFtU'),
             ssl: env.bool(true),
         },
     },
